14b1fe0918eb28542f6099b51a266765735d0c6e
liruiqi123 <1595854626@qq.com>
2018-11-11 00:07:46 +0800

    ###添加轮播图 跳转事件 ###跳转到新的活动说明页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,13 +7,27 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    swiperCurrent: 0,
     movies: [
-      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/1.jpeg' },
-      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/2.jpeg' },
-      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/3.jpeg' },
-      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/4.jpeg' },
-      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/5.jpeg' }
+      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/1.jpeg', key:1},
+      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/2.jpeg', key:2},
+      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/3.jpeg', key:3},
+      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/4.jpeg', key:4},
+      { url: 'https://www.niftydose.cn/Images/wos/index/swiper/5.jpeg', key:5}
     ]
+  },
+
+
+  swiperChange: function (e) {
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
+  swipclick: function (e) {//点击图片触发事件
+    console.log(this.data.swiperCurrent);
+    wx.navigateTo({
+      url: '../../pages/activety/activety',
+    })
   },
   
 })
